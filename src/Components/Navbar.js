c115391305595547eca69d345224984b3cf9ee9d
Vijay <88140072+vijaydudve@users.noreply.github.com>
2023-08-12 01:08:34 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,6 @@
-import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 export class Navbar extends Component {
-    static propTypes = {}
-
     render() {
         return (
             <div>
